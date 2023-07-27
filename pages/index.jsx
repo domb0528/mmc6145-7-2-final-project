c85283b8eb9f4752bd8137dd90f15853db7344c0
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-07-27 17:37:34 -0600

    made minor adjustments to a great start

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,11 +9,11 @@ export default function Home() {
       <Head>
         <title>Calorie Watcher 🏋️‍♂️</title>
         <meta name="description" content="Sip & Savor is a wine pairing app" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍷</text></svg>"/>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏋️‍♂️</text></svg>"/>
       </Head>
       <main>
         <div className={styles.container}>
-          <h1>Welcome to Calorie Watcher where you put in your amount of calories on search page </h1>
+          <h1>Welcome to Calorie Watcher 🏋️‍♂️ where you can find the best meals for weight loss</h1>
           <section className={styles.jumbo}>
             <p>Find a meal plan 🏋️‍♂️</p>
           </section>
